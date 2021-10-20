f2954a5789a4f9bcac45af40e430f5d004561efb
Mignoe <miguel.oliver.2003@hotmail.com>
2021-10-20 11:12:21 -0300

    Uploading my games

---
+++ Games/Minecraft-Snake-Game/scripts/variables.js
@@ -0,0 +1,32 @@
+console.log ("1");
+var end = 'off';
+var zone = []; 
+var start = Math.floor(Math.random() * 1024);
+var start2 = Math.floor(Math.random() * 1024);
+var zoneZ = [];
+var position = start;
+var position2 = start2;
+var i1=0;
+var i2=0;
+var tester;
+var tester2;
+var applePos;
+var P1 = 0; 
+var P2 = 0;
+var trail1 = [];
+var trail2 = [];
+
+
+
+const map = document.getElementById("game");
+var up;
+var down;
+var left;
+var right;
+var W;
+var S;
+var A;
+var D;
+
+
+
